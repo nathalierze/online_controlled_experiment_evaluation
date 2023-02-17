0972529919f11eeb82d29a2acc27c388e46f2a08
nathalierze <nathalie.rzepka@htw-berlin.de>
2023-02-17 11:01:52 +0100

    refactor feature engineering

--- 02_featureEngineering/012_competence_r.r
+++ 02_featureEngineering/012_competence_r.r
@@ -1,61 +1,50 @@
+# calculate rasch model
 #!/bin/bash
 
 install.packages("eRm", repos = "http://cran.us.r-project.org")
 install.packages("ltm", repos = "http://cran.us.r-project.org")
-#install.packages("difR", repos = "http://cran.us.r-project.org")
 install.packages("readxl", repos = "http://cran.us.r-project.org")
 
 library("eRm")
 library("ltm")
-#library("difR")
 library("readxl")
 
 # import and preprocess
 data <- read.csv(file='learning_data.csv')
 
 print('read')
-#print(summary(data))
+print(summary(data))
 
 namen <- data$UserID
 data$UserID <- NULL
 rownames(data) <- namen
 #data <- data[0:50,0:800]
 data <- as.data.frame(data)#apply(data, 2, as.numeric)
-#print(summary(data))
-write.csv(data,"t.csv")
-
-print('rasch')
+write.csv(data,"temp.csv")
 
 #rasch model
 result <- RM(data)
 summary(result)
 
-print('save')
-
 #save results
 options(max.print = 30000)       
 out <- capture.output(summary(result))
 cat("result", out, file="summary_result.txt", sep="\n")
-
 #plot result
 #plotjointICC(result)
 
-print('pp')
-
 #calc person parameter
-pp <- person.parameter(result)
-#plot(pp)
-summary(pp)
-
+personparameters <- person.parameter(result)
+plot(personparameters)
+summary(personparameters)
 
 #LRtest(result)
 #qchisq(0.95,df=21)
 
-print('save')
 #save results
 options(max.print = 30000)       
-out <- capture.output(summary(pp))
-cat("pp", out, file="summary_pp.txt", sep="\n")
+out <- capture.output(summary(personparameters))
+cat("personparameters", out, file="summary_pp.txt", sep="\n")
 
 
 
